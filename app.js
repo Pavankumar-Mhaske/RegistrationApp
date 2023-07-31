726dc28172dfe53b2f105a6927b562c68c04ee6c
P1kumarMhaske <104865937+Pavankumar-Mhaske@users.noreply.github.com>
2023-07-31 19:19:17 +0000

    extensions and route folder created

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const userRouter = require('./routes/userRoutes');
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-}
-);
+app.get('/', userRouter);
 
 module.exports = app;
---
+++ routes/userRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.send('Hello World!');
+}
+);
+
+module.exports = router;
+
