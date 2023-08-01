c05daddfbc729010bb5bcc88bf834c9f8b55d8a2
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-08-01 11:14:25 +0530

    created user

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,33 +2,33 @@
 // const User = require('../models/userModel.js');
 
 exports.home = (req, res) => {
-    res.send('Hello World!');
+  res.send("Hello World!");
 };
 
-// exports.createUser = async (req, res) => {
-//     try {
-//         const {name, email} = req.body;
-//         if(!name || !email) {
-//             throw new Error('Name and email are required');
-//         }
+exports.createUser = async (req, res) => {
+  try {
+    const { name, email } = req.body;
+    if (!name || !email) {
+      throw new Error("Name and email are required");
+    }
 
-//         const userExist = await User.findOne({email});
-//         if(userExist) {
-//             throw new Error('User already exists');
-//         }
+    const userExist = await User.findOne({ email });
+    if (userExist) {
+      throw new Error("User already exists");
+    }
 
-//         const user = await User.create({name, email});
-//         res.status(201).json({
-//             status: 'success',
-//             data: {
-//                 user
-//             },
-//             messege: 'User created successfully'
-//         });
-//     } catch (error) {
-//         res.status(400).json({
-//             status: 'fail',
-//             messege: error.message
-//         });
-//     }
-// };
+    const user = await User.create({ name, email });
+    res.status(201).json({
+      status: "success",
+      data: {
+        user,
+      },
+      messege: "User created successfully",
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      messege: error.message,
+    });
+  }
+};
