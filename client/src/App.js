6bd4395a05743aedc9d354cca365bd873952568b
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2023-08-23 10:09:27 +0530

    remove / from baseurl

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import "./App.css";
 import { Form } from "./components/Form";
 import { UserList } from "./components/UsersList";
 
-const BASE_URL = "https://registrationapp-production.up.railway.app/";
+const BASE_URL = "https://registrationapp-production.up.railway.app";
 
 function App() {
   const [userData, setUserData] = useState(null);
